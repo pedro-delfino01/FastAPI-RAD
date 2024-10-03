02dae90bddfa1db544212a0cd176f692a2e19ad6
Pedro Delfino <pedro.delfino.dev@outlook.com>
2024-10-03 20:16:11 -0300

    add config

---
+++ settings/api_client.py
@@ -0,0 +1,12 @@
+import requests
+
+class APIClient:
+    def __init__(self):
+        self.api_url = Config.FLASK_API_URL
+
+    def send_data(self, data):
+        try:
+            response = requests.post(self.api_url, json=data)
+            response.raise_for_status()
+        except requests.exceptions.RequestException as e:
+            raise Exception(f"Erro ao enviar dados: {e}")
